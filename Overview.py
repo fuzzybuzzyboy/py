aa04320b81ab37cd39fef64e40213aa9718b38c8
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-02-04 20:50:01 +0200

    uhhh hope this works

--- Overview.py
+++ Overview.py
@@ -8,12 +8,10 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     menu_items={'Get help': 'https://github.com/fuzzybuzzyboy/py', 'Report a bug': "https://github.com/fuzzybuzzyboy/py", 'About': "Random items generator for fortnite (no this doesn't inject into your game and do something blah blah blah)"})
 
-Folder_Path = "configs"
-File_Path = os.path.join(Folder_Path, "DeleteThisFile.txt")
-
-if os.path.exists(f"{File_Path}.txt"):
-    print('Deleted file.')
-    os.remove(f"{File_Path}.txt")
+def main():
+    col1, col2 = st.columns(2)
+    with col1: st.image(os.path.join("Pictures", "Example1.jpg"), caption='Example 1 (Loadout generator)', use_column_width=True)
+    with col2: st.image(os.path.join("Pictures", "Example2.jpg"), caption='Example 2 (Loadout generator)', use_column_width=True)
 
 st.title('Hi')
 st.divider()
@@ -24,9 +22,9 @@ st.write("### [+] Added medallions. (Loadout generator, Custom loadout)")
 st.write("### [:] Shorter code (Loadout generator, Custom loadout)")
 st.write('### [:] Nice ui now (Loadout generator, Custom loadout)')
 st.write("### [+] Loadout generator loadout now shows medallions amount and other.")
-col1,col2=st.columns(2)
-with col1: st.image(os.path.join("pictures", "Example1.png"), caption='Example 1 (Loadout generator)', use_column_width=True)
-with col2: st.image(os.path.join("pictures", "Example2.png"), caption='Example 2 (Loadout generator)', use_column_width=True)
+if __name__ == "__main__":
+    main()
+else: pass
 st.write('# \n ##### [/] = Fixed\n ##### [:] = Changed something\n ##### [+] = Added\n ##### [-] = Removed\n\n\n ##### also pls tell me about bugs and other things so i can fix them (on discord) :)')
 
 with st.sidebar:
