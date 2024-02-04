2a3cf40fd3bb1c4c85dff163e08dea4400e7b967
Fuzzy <102690699+fuzzybuzzyboy@users.noreply.github.com>
2024-02-04 20:23:15 +0200

    maybe this fix streamlit app idm

--- Overview.py
+++ Overview.py
@@ -25,8 +25,8 @@ st.write("### [:] Shorter code (Loadout generator, Custom loadout)")
 st.write('### [:] Nice ui now (Loadout generator, Custom loadout)')
 st.write("### [+] Loadout generator loadout now shows medallions amount and other.")
 col1,col2=st.columns(2)
-with col1: st.image(os.path.join("Pictures", "Example1.png"), caption='Example 1 (Loadout generator)', use_column_width=True)
-with col2: st.image(os.path.join("Pictures", "Example2.png"), caption='Example 2 (Loadout generator)', use_column_width=True)
+with col1: st.image(os.path.join("pictures", "Example1.png"), caption='Example 1 (Loadout generator)', use_column_width=True)
+with col2: st.image(os.path.join("pictures", "Example2.png"), caption='Example 2 (Loadout generator)', use_column_width=True)
 st.write('# \n ##### [/] = Fixed\n ##### [:] = Changed something\n ##### [+] = Added\n ##### [-] = Removed\n\n\n ##### also pls tell me about bugs and other things so i can fix them (on discord) :)')
 
 with st.sidebar:
